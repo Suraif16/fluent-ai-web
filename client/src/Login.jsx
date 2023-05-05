e4f34c1acc24fc60486bb2001b68b578c90fc12c
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-05-06 00:04:26 +0530

    logo

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -22,16 +22,21 @@ export const Login = (props) => {
     }
 
     return (
-        <div className="auth-form-container">
-            <h2>Login</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label htmlFor="password">password</label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit">Log In</button>
-            </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        <div>
+            <div className="logo">
+                <img src="../public/logo.png" alt="Logo" />
+            </div>
+            <div className="auth-form-container">
+                <h2>Login</h2>
+                <form className="login-form" onSubmit={handleSubmit}>
+                    <label htmlFor="email">email</label>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                    <label htmlFor="password">password</label>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                    <button type="submit">Log In</button>
+                </form>
+                <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+            </div>
         </div>
     )
 }
